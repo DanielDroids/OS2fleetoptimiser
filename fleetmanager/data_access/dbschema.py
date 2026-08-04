57c3aa022a20b394d18d8449a9be011cb9b1812e
Daniel <panksxd@gmail.com>
2026-08-04 16:13:59 +0200

    add alembic setup: build_dsn, env.py, naming convention

--- fleetmanager/data_access/dbschema.py
+++ fleetmanager/data_access/dbschema.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, MetaData
 from sqlalchemy.orm import (
     relationship,
     Mapped,
@@ -12,8 +12,15 @@ from datetime import datetime
 
 
 class Base(MappedAsDataclass, DeclarativeBase):
-    pass
-
+    metadata = MetaData(
+        naming_convention={
+            "ix": "ix_%(column_0_label)s",
+            "uq": "uq_%(table_name)s_%(column_0_name)s",
+            "ck": "ck_%(table_name)s_%(constraint_name)s",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s",
+        }
+    )
 
 class Trips(Base):
     __tablename__ = "trips"
